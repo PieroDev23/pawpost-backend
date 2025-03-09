0b7fe3590379407d7601876a9801280e4811c5a4
PieroDev23 <pierodavilaaguirre22@gmail.com>
2025-03-08 21:41:40 -0500

    feat: modify application

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ import serverlessExpress from "@codegenie/serverless-express";
 const pawpostApp = new PawPostApp();
 const app = pawpostApp.app.callback();
 
+pawpostApp.run();
+
 // setup the lambda code
 export const handler = serverlessExpress({ app });
